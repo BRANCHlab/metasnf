286ded0256297dfe20c4c92df84f764d89d4d62c
Prashanth Velayudhan <psvelayu@gmail.com>
2022-10-19 14:14:01 -0400

    rename function works even if some otbi01 columns are missing

--- R/rename_otbi01.R
+++ R/rename_otbi01.R
@@ -24,46 +24,48 @@
 #' otbi01_renamed <- rename_otbi01(abcd_otbi01)
 rename_otbi01 <- function(otbi01) {
     renamed_otbi01 <- otbi01 |>
-        dplyr::rename(
-            "hosp_er_inj" = "tbi_1", # ever hospitalized/ER for head/neck injury?
-            "hosp_er_loc" = "tbi_1b", # if LOC, how long?
-            "hosp_er_mem_daze" = "tbi_1c", # were they dazed or have memory gap?
-            "hosp_er_age" = "tbi_1d", # how old were they?
-            "vehicle_inj" = "tbi_2", # ever injured in a vehicle accident?
-            "vehicle_loc" = "tbi_2b", # if LOC, how long?
-            "vehicle_mem_daze" = "tbi_2c", # were they dazed or have memory gap?
-            "vehicle_age" = "tbi_2d", # how old were they?
-            "fall_hit_inj" = "tbi_3", # ever injured head/neck from fall or hit?
-            "fall_hit_loc" = "tbi_3b", # if LOC, how long?
-            "fall_hit_mem_daze" = "tbi_3c", # were they dazed or have memory gap?
-            "fall_hit_age" = "tbi_3d", # how old were they?
-            "violent_hit_inj" = "tbi_4", # ever injure head/neck from violence?
-            "violent_hit_loc" = "tbi_4b", # if LOC, how long?
-            "violent_hit_mem_daze" = "tbi_4c", # were they dazed or have memory gap?
-            "violent_hit_age" = "tbi_4d", # how old were they?
-            "blast_inj" = "tbi_5", # ever injure head or neck from blast?
-            "blast_loc" = "tbi_5b", # if LOC, how long?
-            "blast_mem_daze" = "tbi_5c", # were they dazed or have memory gap?
-            "blast_age" = "tbi_5d", # how old were they?
-            "other_loc_inj" = "tbi_6o", # any other injuries with LOC?
-            "other_loc_num" = "tbi_6p", # how many more?
-            "other_loc_max_loc_mins" = "tbi_6q", # how long was longest LOC?
-            "other_loc_num_over_30" = "tbi_6r", # how many were >= 30 min?
-            "other_loc_min_age" = "tbi_6s", # what was their youngest age?
-            "multi_inj" = "tbi_7a", # did they have a period of multiple injuries?
-            "multi_loc" = "tbi_7c1", # if LOC, how long?
-            "multi_mem_daze" = "tbl_7c2", # were they dazed or have memory gap?
-            "multi_effect_start_age" = "tbi_7e", # at what age did the effects begin?
-            "multi_effect_end_age" = "tbi_7f", # at what age did the effects end?
-            "other_multi_inj" = "tbi_7g", # was there another multiple injury period?
-            "other_multi_effect_type" = "tbi_7i", # typical effect of the injury?
-            "other_multi_effect_start_age" = "tbi_7k", # start age of those effects?
-            "other_multi_effect_end_age" = "tbi_7l", # end age of those effects?
-            "other_other_multi_inj" = "tbi_8g", # another period of multiple inj?
-            "other_other_multi_effect_type" = "tbi_8i", # typical effects?
-            "other_other_multi_effect_start_age" = "tbi_8k", # start age of effects?
-            "other_other_multi_effect_end_age" = "tbi_8l" # end age of effects?
-        )
+        dplyr::rename_with(
+            ~ dplyr::case_when(
+                . == "tbi_1" ~ "hosp_er_inj",
+                . == "tbi_1b" ~ "hosp_er_loc",
+                . == "tbi_1c" ~ "hosp_er_mem_daze",
+                . == "tbi_1d" ~ "hosp_er_age",
+                . == "tbi_2" ~ "vehicle_inj",
+                . == "tbi_2b" ~ "vehicle_loc",
+                . == "tbi_2c" ~ "vehicle_mem_daze",
+                . == "tbi_2d" ~ "vehicle_age",
+                . == "tbi_3" ~ "fall_hit_inj",
+                . == "tbi_3b" ~ "fall_hit_loc",
+                . == "tbi_3c" ~ "fall_hit_mem_daze",
+                . == "tbi_3d" ~ "fall_hit_age",
+                . == "tbi_4" ~ "violent_hit_inj",
+                . == "tbi_4b" ~ "violent_hit_loc",
+                . == "tbi_4c" ~ "violent_hit_mem_daze",
+                . == "tbi_4d" ~ "violent_hit_age",
+                . == "tbi_5" ~ "blast_inj",
+                . == "tbi_5b" ~ "blast_loc",
+                . == "tbi_5c" ~ "blast_mem_daze",
+                . == "tbi_5d" ~ "blast_age",
+                . == "tbi_6o" ~ "other_loc_inj",
+                . == "tbi_6p" ~ "other_loc_num",
+                . == "tbi_6q" ~ "other_loc_max_loc_mins",
+                . == "tbi_6r" ~ "other_loc_num_over_30",
+                . == "tbi_6s" ~ "other_loc_min_age",
+                . == "tbi_7a" ~ "multi_inj",
+                . == "tbi_7c1" ~ "multi_loc",
+                . == "tbl_7c2" ~ "multi_mem_daze",
+                . == "tbi_7e" ~ "multi_effect_start_age",
+                . == "tbi_7f" ~ "multi_effect_end_age",
+                . == "tbi_7g" ~ "other_multi_inj",
+                . == "tbi_7i" ~ "other_multi_effect_type",
+                . == "tbi_7k" ~ "other_multi_effect_start_age",
+                . == "tbi_7l" ~ "other_multi_effect_end_age",
+                . == "tbi_8g" ~ "other_other_multi_inj",
+                . == "tbi_8i" ~ "other_other_multi_effect_type",
+                . == "tbi_8k" ~ "other_other_multi_effect_start_age",
+                . == "tbi_8l" ~ "other_other_multi_effect_end_age",
+                TRUE ~ .)
+            )
     if (identical(renamed_otbi01, otbi01)) {
         warning("No changes were made to the object.")
     }
