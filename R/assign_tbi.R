78be9075714bd39ffc20f7dc9ca52e4423c43aa2
psvelayudhan <psvelayu@gmail.com>
2022-10-31 15:04:26 -0400

    integrate the bulk of acute symptom's preprocessing

--- R/assign_tbi.R
+++ R/assign_tbi.R
@@ -17,44 +17,47 @@
 #'
 assign_tbi <- function(tbi_df) {
     print("Note: This function converts the mTBI column types to numeric.")
+
     # Assign column types
-    dft <- tbi_df |>
+    dfct <- tbi_df |>
         dplyr::mutate(
             dplyr::across(
                 "hosp_er_inj":"other_other_multi_effect_end_age", as.numeric))
-    dft$"interview_age" <- as.numeric(dft$"interview_age")
+    dfct$"interview_age" <- as.numeric(dfct$"interview_age")
+
     # Generate mtbi and moderate_or_severe_tbi columns
-    dfm <- dft |> dplyr::mutate(
+    dfm <- dfct |> dplyr::mutate(
         mtbi = dplyr::case_when(
-            (dft$"hosp_er_loc" < 2 & dft$"hosp_er_mem_daze" == 1) |
-                dft$"hosp_er_loc" == 1 ~ 1,
-            (dft$"vehicle_loc" < 2 & dft$"vehicle_mem_daze" == 1) |
-                dft$"vehicle_loc" == 1 ~ 1,
-            (dft$"fall_hit_loc" < 2 & dft$"fall_hit_mem_daze" == 1) |
-                dft$"fall_hit_loc" == 1 ~ 1,
-            (dft$"violent_loc" < 2 & dft$"violent_mem_daze" == 1) |
-                dft$"violent_loc" == 1 ~ 1,
-            (dft$"blast_loc" < 2 & dft$"blast_mem_daze" == 1) |
-                dft$"blast_loc" == 1 ~ 1,
-            (dft$"other_loc_num" - dft$"other_loc_num_over_30") > 0 ~ 1,
-            (dft$"multi_loc" < 2 & dft$"multi_mem_daze" == 1) |
-                dft$"multi_loc" == 1 ~ 1,
-            dft$"other_multi_inj" == 1 ~ 0.5,
-            dft$"other_other_multi_inj" == 1 ~ 0.5,
+            (dfct$"hosp_er_loc" < 2 & dfct$"hosp_er_mem_daze" == 1) |
+                dfct$"hosp_er_loc" == 1 ~ 1,
+            (dfct$"vehicle_loc" < 2 & dfct$"vehicle_mem_daze" == 1) |
+                dfct$"vehicle_loc" == 1 ~ 1,
+            (dfct$"fall_hit_loc" < 2 & dfct$"fall_hit_mem_daze" == 1) |
+                dfct$"fall_hit_loc" == 1 ~ 1,
+            (dfct$"violent_loc" < 2 & dfct$"violent_mem_daze" == 1) |
+                dfct$"violent_loc" == 1 ~ 1,
+            (dfct$"blast_loc" < 2 & dfct$"blast_mem_daze" == 1) |
+                dfct$"blast_loc" == 1 ~ 1,
+            (dfct$"other_loc_num" - dfct$"other_loc_num_over_30") > 0 ~ 1,
+            (dfct$"multi_loc" < 2 & dfct$"multi_mem_daze" == 1) |
+                dfct$"multi_loc" == 1 ~ 1,
+            dfct$"other_multi_inj" == 1 ~ 0.5,
+            dfct$"other_other_multi_inj" == 1 ~ 0.5,
             TRUE ~ 0),
         moderate_or_severe_tbi = dplyr::case_when(
-            (dft$"hosp_er_loc" > 1 |
-                dft$"vehicle_loc" > 1 |
-                dft$"fall_hit_loc" > 1 |
-                dft$"violent_loc" > 1 |
-                dft$"blast_loc" > 1 |
-                dft$"other_loc_num_over_30" > 0 |
-                dft$"multi_loc" > 1) ~ 1,
-            dft$"other_multi_inj" == 1 ~ 0.5,
-            dft$"other_other_multi_inj" == 1 ~ 0.5,
+            (dfct$"hosp_er_loc" > 1 |
+                dfct$"vehicle_loc" > 1 |
+                dfct$"fall_hit_loc" > 1 |
+                dfct$"violent_loc" > 1 |
+                dfct$"blast_loc" > 1 |
+                dfct$"other_loc_num_over_30" > 0 |
+                dfct$"multi_loc" > 1) ~ 1,
+            dfct$"other_multi_inj" == 1 ~ 0.5,
+            dfct$"other_other_multi_inj" == 1 ~ 0.5,
             TRUE ~ 0))
+
     # Generate columns indicating which injury types were mTBIs
-    dfl <- dfm |> dplyr::mutate(
+    dfm2 <- dfm |> dplyr::mutate(
         hosp_er_mtbi = dplyr::case_when((
                 dfm$"hosp_er_loc" < 2 &
                 dfm$"hosp_er_mem_daze" == 1) |
@@ -84,70 +87,146 @@ assign_tbi <- function(tbi_df) {
                 dfm$"multi_loc" < 2 & dfm$"multi_mem_daze" == 1) |
                 dfm$"multi_loc" == 1 ~ 1,
                 TRUE ~ 0))
+
     # Scale injury ages to match interview ages if necessary
-    if (mean(dfl$"hosp_er_age", na.rm = TRUE) < 20) {
+    if (mean(dfm2$"hosp_er_age", na.rm = TRUE) < 20) {
         print(paste0("Note: Interview ages are provided in months, but ",
             "injury ages are provided in years. Injury ages will be scaled ",
             "up to determine time between interview and latest mTBI."))
-        dfl$"blast_age" <-
-            dfl$"blast_age" * 12
-        dfl$"hosp_er_age" <-
-            dfl$"hosp_er_age" * 12
-        dfl$"vehicle_age" <-
-            dfl$"vehicle_age" * 12
-        dfl$"fall_hit_age" <-
-            dfl$"fall_hit_age" * 12
-        dfl$"violent_age" <-
-            dfl$"violent_age" * 12
-        dfl$"other_loc_min_age" <-
-            dfl$"other_loc_min_age" * 12
-        dfl$"multi_effect_end_age" <-
-            dfl$"multi_effect_end_age" * 12
+        dfm2$"blast_age" <-
+            dfm2$"blast_age" * 12
+        dfm2$"hosp_er_age" <-
+            dfm2$"hosp_er_age" * 12
+        dfm2$"vehicle_age" <-
+            dfm2$"vehicle_age" * 12
+        dfm2$"fall_hit_age" <-
+            dfm2$"fall_hit_age" * 12
+        dfm2$"violent_age" <-
+            dfm2$"violent_age" * 12
+        dfm2$"other_loc_min_age" <-
+            dfm2$"other_loc_min_age" * 12
+        dfm2$"multi_effect_end_age" <-
+            dfm2$"multi_effect_end_age" * 12
     }
+
     # Generate columns indicating time since mTBI of specific type
-    dft1 <- dfl |> dplyr::mutate(
+    dft <- dfm2 |> dplyr::mutate(
         hosp_er_mtbi_mpi = dplyr::case_when(
-            dfl$"hosp_er_mtbi" == 1 ~
-                dfl$"interview_age" - dfl$"hosp_er_age"),
+            dfm2$"hosp_er_mtbi" == 1 ~
+                dfm2$"interview_age" - dfm2$"hosp_er_age"),
         vehicle_mtbi_mpi = dplyr::case_when(
-            dfl$"vehicle_mtbi" == 1 ~
-                dfl$"interview_age" - dfl$"vehicle_age"),
+            dfm2$"vehicle_mtbi" == 1 ~
+                dfm2$"interview_age" - dfm2$"vehicle_age"),
         fall_hit_mtbi_mpi = dplyr::case_when(
-            dfl$"fall_hit_mtbi" == 1 ~
-                dfl$"interview_age" - dfl$"fall_hit_age"),
+            dfm2$"fall_hit_mtbi" == 1 ~
+                dfm2$"interview_age" - dfm2$"fall_hit_age"),
         violent_mtbi_mpi = dplyr::case_when(
-            dfl$"violent_mtbi" == 1 ~
-                dfl$"interview_age" - dfl$"violent_age"),
+            dfm2$"violent_mtbi" == 1 ~
+                dfm2$"interview_age" - dfm2$"violent_age"),
         blast_mtbi_mpi = dplyr::case_when(
-            dfl$"blast_mtbi" == 1 ~
-                dfl$"interview_age" - dfl$"blast_age"),
+            dfm2$"blast_mtbi" == 1 ~
+                dfm2$"interview_age" - dfm2$"blast_age"),
         other_loc_mtbi_mpi = dplyr::case_when(
-            dfl$"other_loc_mtbi" == 1 ~
-                dfl$"interview_age" - dfl$"other_loc_min_age"),
+            dfm2$"other_loc_mtbi" == 1 ~
+                dfm2$"interview_age" - dfm2$"other_loc_min_age"),
         multi_mtbi_mpi = dplyr::case_when(
-            dfl$"multi_mtbi" == 1 ~
-                dfl$"interview_age" - dfl$"multi_effect_end_age"))
+            dfm2$"multi_mtbi" == 1 ~
+                dfm2$"interview_age" - dfm2$"multi_effect_end_age"))
+
     # Generate column indicating time since latest mTBI
-    dft2 <- dft1 |>
+    dft2 <- dft |>
         dplyr::mutate(latest_mtbi_mpi = pmin(
-            dft1$"hosp_er_mtbi_mpi",
-            dft1$"vehicle_mtbi_mpi",
-            dft1$"fall_hit_mtbi_mpi",
-            dft1$"violent_mtbi_mpi",
-            dft1$"blast_mtbi_mpi",
-            dft1$"other_loc_mtbi_mpi",
-            dft1$"multi_mtbi_mpi",
+            dft$"hosp_er_mtbi_mpi",
+            dft$"vehicle_mtbi_mpi",
+            dft$"fall_hit_mtbi_mpi",
+            dft$"violent_mtbi_mpi",
+            dft$"blast_mtbi_mpi",
+            dft$"other_loc_mtbi_mpi",
+            dft$"multi_mtbi_mpi",
             na.rm = TRUE
         ))
+
+    # Generate columns indicating age at latest mTBI
+    dfa <- dft2 |>
+        dplyr::mutate(latest_mtbi_age = pmax(
+            dft2$"hosp_er_age",
+            dft2$"vehicle_age",
+            dft2$"fall_hit_age",
+            dft2$"violent_age",
+            dft2$"blast_age",
+            dft2$"other_loc_min_age",
+            dft2$"multi_effect_end_age",
+            na.rm = TRUE
+        ))
+
+
+    # Generate column indicating mechanism of latest mTBI
+    dfmech <- dfa |> dplyr::mutate(latest_mtbi_mechanism = dplyr::case_when(
+        dfa$"latest_mtbi_age" == dfa$"hosp_er_age" ~
+            "hosp_er_age",
+        dfa$"latest_mtbi_age" == dfa$"vehicle_age" ~
+            "vehicle_age",
+        dfa$"latest_mtbi_age" == dfa$"fall_hit_age" ~
+            "fall_hit_age",
+        dfa$"latest_mtbi_age" == dfa$"violent_age" ~
+            "violent_age",
+        dfa$"latest_mtbi_age" == dfa$"blast_age" ~
+            "blast_age",
+        dfa$"latest_mtbi_age" == dfa$"other_loc_min_age" ~
+            "other_loc_min_age",
+        dfa$"latest_mtbi_age" == dfa$"multi_effect_end_age" ~
+            "multi_effect_end_age"
+        ))
+
     # Generate column for best guess of number of mTBIs sustained
-    dfc <- dft2 |>
+    dfnt <- dfmech |>
         dplyr::mutate(mtbi_count = (
-            dft2$"hosp_er_mtbi" +
-            dft2$"vehicle_mtbi" +
-            dft2$"fall_hit_mtbi" +
-            dft2$"violent_mtbi" +
-            dft2$"blast_mtbi" +
-            dft2$"other_loc_mtbi" +
-            dft2$"multi_mtbi"))
-    return(dfc)
+            dfmech$"hosp_er_mtbi" +
+            dfmech$"vehicle_mtbi" +
+            dfmech$"fall_hit_mtbi" +
+            dfmech$"violent_mtbi" +
+            dfmech$"blast_mtbi" +
+            dfmech$"other_loc_mtbi" +
+            dfmech$"multi_mtbi"))
+
+
+    # Create LOC duration variable for the 'other_loc' category
+    dfl <- dfnt |>
+        dplyr::mutate(other_loc_loc = dplyr::case_when(
+            dfnt$"other_loc_max_loc_mins" == 0 ~ 0,
+            dfnt$"other_loc_max_loc_mins" > 0 &
+                dfnt$"other_loc_max_loc_mins" <= 30 ~ 1,
+            dfnt$"other_loc_max_loc_mins" > 30 &
+                dfnt$"other_loc_max_loc_mins" <= 1440 ~ 2,
+            dfnt$"other_loc_max_loc_mins" > 1440 ~ 3,
+            TRUE ~ NA_real_
+        ))
+
+    # Create column indicating loc of latest mTBI
+    dfll <- dfl |>
+        dplyr::mutate(latest_mtbi_loc = dplyr::case_when(
+            dfl$"latest_mtbi_mechanism" == "hosp_er" ~ hosp_er_loc,
+            dfl$"latest_mtbi_mechanism" == "vehicle" ~ vehicle_loc,
+            dfl$"latest_mtbi_mechanism" == "fall_hit" ~ fall_hit_loc,
+            dfl$"latest_mtbi_mechanism" == "violent" ~ violent_loc,
+            dfl$"latest_mtbi_mechanism" == "blast" ~ blast_loc,
+            dfl$"latest_mtbi_mechanism" == "other_loc" ~ other_loc_loc,
+            dfl$"latest_mtbi_mechanism" == "multi" ~ multi_loc,
+            TRUE ~ NA_real_
+        ))
+
+    # Generate column indicating if latest mTBI had memory loss / dazed feeling
+    dfml <- dfll |>
+        dplyr::mutate(latest_mtbi_mem_daze = dplyr::case_when(
+            dfll$"latest_mtbi_mechanism" == "hosp_er" ~ hosp_er_mem_daze,
+            dfll$"latest_mtbi_mechanism" == "vehicle" ~ vehicle_mem_daze,
+            dfll$"latest_mtbi_mechanism" == "fall_hit" ~ fall_hit_mem_daze,
+            dfll$"latest_mtbi_mechanism" == "violent" ~ violent_mem_daze,
+            dfll$"latest_mtbi_mechanism" == "blast" ~ blast_mem_daze,
+            dfll$"latest_mtbi_mechanism" == "other_loc" ~ NA_real_,
+            dfll$"latest_mtbi_mechanism" == "multi" ~ multi_mem_daze,
+            TRUE ~ NA_real_
+        ))
+
+    return(dfml)
 }
