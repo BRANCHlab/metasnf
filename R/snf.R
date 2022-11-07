65530342b432ac81e12b21c1c2010029a39aff3b
Prashanth Velayudhan <psvelayu@gmail.com>
2022-11-07 14:30:55 -0500

    beginning pseudocode for snf design matrix

--- R/snf.R
+++ R/snf.R
@@ -1,6 +1,35 @@
-#' Takes a list of dataframes, orders them, and subsets to their shared subjects
-#'
-#' @param df_list
-#'
-#' @return common_df_list
+# Given list of similarity matrices, return fused network
+snf <- function() {
+    return(NULL)
+}
 
+# Given measurement types, their subdomains, and their domains
+snf_step <- function() {
+    return(NULL)
+}
+
+# Given fused network, return cluster membership of patients
+spectral_clustering <- function() {
+    return(NULL)
+}
+
+# Given clustered subjects and outcome measures, evaluate clustering utility
+evaluate_clustering <- function() {
+    return(NULL)
+}
+
+
+
+iterate_design_matrix <- function() {
+  # build design matrix rows
+    # for each option, randomly select a value
+    # check if that vector of options has been done before
+    # feed those options into SNF
+  # execute design matrix rows
+  # update design matrix output columns
+  return(NULL)
+}
+
+visualize_design_matrix <- function() {
+  return(NULL)
+}
