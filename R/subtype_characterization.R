839b1c2a27b6783e02a27acb694bf828babd00d5
psvelayudhan <psvelayu@gmail.com>
2022-12-19 17:03:59 -0500

    function to be fleshed out for later regarding characterizing the full meta-clustering results

---
+++ R/subtype_characterization.R
@@ -0,0 +1,17 @@
+cluster_distribution <- function() {
+    #subs <- a |>  # a is the result of `execute_design_matrix`
+    #    dplyr::select(starts_with("NDAR"))
+    #subs$cc_zeros <- apply(subs, 1, function(x) length(which(x == 0))/length(x))
+    #subs$cc_ones <- apply(subs, 1, function(x) length(which(x == 1))/length(x))
+    #subs$cc_twos <- apply(subs, 1, function(x) length(which(x == 2))/length(x))
+    #subs$cc_threes <- apply(subs, 1, function(x) length(which(x == 3))/length(x))
+    #subs$cc_fours <- apply(subs, 1, function(x) length(which(x == 4))/length(x))
+    #subs_cc <- subs |> dplyr::select(starts_with("cc"))
+    #subs_cc
+    #cc_nums <- as.numeric(as.matrix(subs_cc))
+    #cc_nums <- cc_nums[cc_nums > 0]
+    #mean(as.matrix(cc_nums))
+    #min(as.matrix(cc_nums))
+    #max(as.matrix(cc_nums))
+    #sd(as.matrix(cc_nums))
+}
