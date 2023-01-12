6c859fc68d110b48f4f7a432ad994f1ae8c67232
Prashanth Velayudhan <psvelayu@gmail.com>
2023-01-12 14:47:44 -0500

    functions for meta-clustering and characterization of clusters

---
+++ R/cluster_characterization.R
@@ -0,0 +1,46 @@
+#' Heatmap p-value matrix
+#'
+#' @param p_val_matrix matrix of p-values
+#' @param file_path where to store heatmap
+#'
+#' @export
+heatmap_pvals <- function(p_val_matrix, file_path = NA) {
+    my_colors <- grDevices::colorRampPalette(c("cyan", "deeppink3"))
+    pheatmap::pheatmap(p_val_matrix, col = rev(my_colors(100)),
+        filename = file_path)
+    pheatmap::pheatmap(p_val_matrix, col = rev(my_colors(100)))
+}
+
+
+#' Select all output_matrix columns except for subjects
+#'
+#' @description
+#' Return a dataframe without any subject columns
+#'
+#' @param df Datframe
+#'
+#' @return df_no_subs Dataframe without subjects
+#'
+#' @export
+no_subs <- function(df) {
+    df_no_subs <- df |> dplyr::select(!(dplyr::starts_with("NDAR")))
+    return(df_no_subs)
+}
+
+
+#' Select only output_matrix columns containing subjects
+#'
+#' @description
+#' Return a dataframe with only subject columns
+#'
+#' @param df Datframe
+#'
+#' @return df_subs Dataframe without subjects
+#'
+#' @export
+subs <- function(df) {
+    df_subs <- df |> dplyr::select(
+        "row_id",
+        dplyr::starts_with("NDAR"))
+    return(df_subs)
+}
---
+++ R/meta_cluster.R
@@ -0,0 +1,45 @@
+#' Calculate adjusted rand index between two rows of output matrix
+#'
+#' Output matrix must have no row_id or non-sub columns
+#'
+#' @param r1 first row of cluster labels
+#' @param r2 second row of cluster labels
+#' @param df dataframe containing cluster labels
+#'
+#' @return ari adjusted rand index
+#'
+#' @export
+calc_ari <- function(r1, r2, df) {
+    v1 <- as.numeric(df[r1, ])
+    v2 <- as.numeric(df[r2, ])
+    ari <- mclust::adjustedRandIndex(v1, v2)
+    return(ari)
+}
+
+
+#' Meta-cluster calculations
+#'
+#' @description
+#' Generate matrix of pairwise cluster-solution similarities by Adjusted Rand
+#'  index calculations
+#'
+#' @param mc_om output_matrix
+#'
+#' @return mc_sm meta-clustering similarity matrix
+#'
+#' @export
+meta_cluster <- function(mc_om) {
+    # Only row id and subject label cols
+    mc_om_subs <- subs(mc_om)
+    # Only subject label cols
+    mc_om_no_id <- mc_om_subs[, 2:length(mc_om_subs)]
+    # The skeleton of the inter-cluster similarity matrix
+    mc_sm <- matrix(0, nrow(mc_om_subs), nrow(mc_om_subs))
+    # Calculating pairwise ARIs across rows
+    for (i in seq_len(nrow(mc_sm))) {
+        for (j in seq_len(ncol(mc_sm))) {
+            mc_sm[i, j] <- calc_ari(i, j, mc_om_no_id)
+        }
+    }
+    return(mc_sm)
+}
