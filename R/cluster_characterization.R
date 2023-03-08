1d2ee79b720bcad70d1f6d1208973f7959f6a50b
Prashanth Velayudhan <psvelayu@gmail.com>
2023-03-08 16:33:55 -0500

    add functions to obtain a cluster and subjectkey from an OM row and to calculate p-values given a characterization DF

--- R/cluster_characterization.R
+++ R/cluster_characterization.R
@@ -57,3 +57,45 @@ select_om <- function(om, row_ids) {
     selected_om <- om[om$"row_id" %in% row_ids, ]
     return(selected_om)
 }
+
+
+#' Extract dataframe of cluster and subject key from output matrix row
+#'
+#' @param om_row Output matrix row
+#'
+#' @return cluster_df dataframe of cluster and subjectkey
+#'
+#' @export
+get_cluster_df <- function(om_row) {
+    cluster_df <-
+        subs(om_row) |>
+        t() |>
+        data.frame()
+    cluster_df$id <- rownames(cluster_df)
+    rownames(cluster_df) <- NULL
+    colnames(cluster_df) <- c("cluster", "subjectkey")
+    cluster_df <- cluster_df[2:nrow(cluster_df), ]
+    return(cluster_df)
+}
+
+
+
+#' Calculate overall p-values for a characterization_df
+#'
+#' @param characterization_df A merged list containing cluster, subjectkey, and
+#'  various CBCL outcomes
+#'
+#' @export
+cbcl_ord_reg <- function(characterization_df) {
+    outcomes <- characterization_df |>
+        dplyr::select(dplyr::starts_with("cbcl")) |>
+        colnames()
+    print("Ordinal regression p-values:")
+    print("----------------------------")
+    for (outcome in outcomes) {
+        outcome_df <- characterization_df[, c("subjectkey", outcome)]
+        cluster_df <- characterization_df[, c("cluster", "subjectkey")]
+        pval <- signif(ord_reg_p(cluster_df, outcome_df, outcome), 2)
+        print(paste0(outcome, ": ", format(pval, scientific = FALSE)))
+    }
+}
