232d333c1849db963e3583d95b071a909fe134c2
psvelayudhan <psvelayu@gmail.com>
2023-03-09 15:26:56 -0500

    calc nmi function

--- R/cluster_characterization.R
+++ R/cluster_characterization.R
@@ -131,3 +131,41 @@ cbcl_anova <- function(characterization_df, bonferroni = FALSE) {
         print(paste0(outcome, ": ", signif(pval, 2)))
     }
 }
+
+
+#' Calculate NMI scores for SNF inputs
+#'
+#' @param om_row An output matrix row
+#' @param data_list A data list
+#'
+#' @return nmi_df A dataframe containing input NMI scores in descending order
+#'
+#' @export
+calc_nmi <- function(om_row, data_list) {
+    K <- om_row$"K"
+    alpha <- om_row$"alpha"
+    nclust <- om_row$"nclust"
+    # Getting the clustering results of the full fused network
+    full_solution <- as.numeric(subs(om_row)[, -1])
+    # Getting the clustering results of each individual SNF input
+    dist_list <- lapply(data_list,
+        function(x) {
+            get_dist_matrix(df = x$"data", input_type = x$"type")
+        })
+    sim_list <- lapply(dist_list,
+        function(x) {
+            SNFtool::affinityMatrix(x, K = K, sigma = alpha)
+        })
+    input_solutions <- lapply(sim_list,
+        function(x) {
+            SNFtool::spectralClustering(x, nclust)
+        })
+    nmi_scores <- lapply(input_solutions,
+        function(x) {
+            SNFtool::calNMI(full_solution, x)
+        })
+    nmi_scores <- format(signif(unlist(nmi_scores), 2), scientific = FALSE)
+    nmi_df <- data.frame(input = sdl(data_list)$"name", nmi = nmi_scores)
+    nmi_df <- nmi_df |> dplyr::arrange(dplyr::desc(nmi_df$"nmi"))
+    return(nmi_df)
+}
