c6ca6ae6bbc643a469dd8e7c9cdb446c45172b6f
Prashanth Velayudhan <psvelayu@gmail.com>
2023-03-15 16:39:58 -0400

    add modified forms of SNFtool functions to handle floating point errors during affinity matrix calculations

--- R/cluster_characterization.R
+++ R/cluster_characterization.R
@@ -180,14 +180,16 @@ calc_nmi <- function(om_row, data_list) {
         function(x) {
             SNFtool::affinityMatrix(x, K = K, sigma = alpha)
         })
-    # the problem spot
-    return(sim_list)
     input_solutions <- lapply(sim_list,
         function(x) {
-            print(head(x))
-            SNFtool::spectralClustering(x, nclust)
+            tryCatch({
+                SNFtool::spectralClustering(x, nclust)
+            },
+            error = function(cond) {
+                spectral_clustering(x, nclust)
+            }
+            )
         })
-    print(5)
     nmi_scores <- lapply(input_solutions,
         function(x) {
             SNFtool::calNMI(full_solution, x)
@@ -212,7 +214,6 @@ calc_nmi <- function(om_row, data_list) {
 #' @export
 om_to_nmi_df <- function(om, data_list) {
     for (row in seq_len(nrow(om))) {
-        print(om[row, ]$"row_id")
         if (row == 1) {
             nmi_df <- calc_nmi(om[row, ], data_list)
             colnames(nmi_df) <- c("input", om$"significance"[row])
