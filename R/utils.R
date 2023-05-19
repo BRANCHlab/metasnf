9b3311665c356de33b0d21459871f933441baf49
psvelayudhan <psvelayu@gmail.com>
2023-05-19 17:33:41 -0400

    addition of train/test splitting functions from abcdutils

--- R/utils.R
+++ R/utils.R
@@ -112,4 +112,51 @@ merge_df_list <- function(df_list, join = "inner") {
     return(merged_df)
 }
 
+#' Training and testing split
+#'
+#' @description
+#' Given a vector of subject_id and a threshold, returns a list of which members
+#'  should be in the training set and which should be in the testing set. The
+#'  function relies on whether or not the absolute value of the Jenkins's
+#'  one_at_a_time hash function exceeds the maximum possible value
+#'  (2147483647) multiplied by the threshold
+#'
+#' @param train_frac The fraction (0 to 1) of subjects for training
+#' @param subjects The available subjects for distribution
+#'
+#' @return split a named list containing the training and testing subject_ids
+#'
+#' @export
+train_test_assign <- function(train_frac, subjects) {
+    train_thresh <- 2147483647 * train_frac
+    train <-
+        subjects[abs(digest::digest2int(subjects, seed = 42)) < train_thresh]
+    test <-
+        subjects[abs(digest::digest2int(subjects, seed = 42)) >= train_thresh]
+    train_df <- data.frame(subjectkey = train, split = "train")
+    test_df <- data.frame(subjectkey = test, split = "test")
+    assigned_df <- rbind(train_df, test_df)
+    return(assigned_df)
+}
 
+#' Filter data to training or testing subjects only
+#'
+#' @description
+#' Given a dataframe, assigned_df object (from train_test_split()), and split,
+#'  return just the data for subjects that were assigned the specified split
+#'
+#' @param df Dataframe to be subsetted into training or testing split
+#' @param assigned_df Dataframe containing "subjectkey" and "split" cols
+#' @param split Split to keep ("train" or "test")
+#'
+#' @return split_df
+#'
+#' @export
+keep_split <- function(df, assigned_df, split) {
+    train_or_test <- split
+    split_df <- assigned_df |>
+        dplyr::filter(split == train_or_test) |>
+        dplyr::inner_join(df, by = "subjectkey") |>
+        dplyr::select(-split)
+    return(split_df)
+}
