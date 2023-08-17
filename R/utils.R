fdd4397e5d498ead00802450ef1b163ac110446c
psvelayudhan <psvelayu@gmail.com>
2023-08-17 15:25:19 -0400

    prep for release 1.0: improve documentation for keep_split(), remove unneeded function list_remove()

--- R/utils.R
+++ R/utils.R
@@ -190,14 +190,15 @@ train_test_assign <- function(train_frac, subjects, seed = 42) {
 #' Filter data to training or testing subjects only
 #'
 #' @description
-#' Given a dataframe, assigned_df object (from train_test_split()), and split,
-#'  return just the data for subjects that were assigned the specified split
+#' Given a dataframe and the results of `train_test_split()`, return just the
+#'  data for subjects that were assigned the specified split
 #'
 #' @param df Dataframe to be subsetted into training or testing split
-#' @param assigned_df Dataframe containing "subjectkey" and "split" cols
-#' @param split Split to keep ("train" or "test")
+#' @param assigned_df Dataframe containing "subjectkey" and "split" cols from
+#'  `train_test_assign()`
+#' @param split String indicating which split to keep ("train" or "test")
 #'
-#' @return split_df
+#' @return split_df Dataframe subsetted to specified split
 #'
 #' @export
 keep_split <- function(df, assigned_df, split) {
@@ -207,33 +208,4 @@ keep_split <- function(df, assigned_df, split) {
         dplyr::inner_join(df, by = "subjectkey") |>
         dplyr::select(-split)
     return(split_df)
-}
-
-#' Remove items from a data_list or outcome_list
-#'
-#' Removes specified elements from a provided data_list or outcome_list object
-#'
-#' @param list_object The data_list or outcome_list containing components to be
-#'  removed
-#' @param ... Any number of components to remove from the list object, passed as
-#'  strings
-#'
-#' @return pruned_list The pruned list object
-#'
-#' @export
-list_remove <- function(list_object, ...) {
-    to_remove <- list(...)
-    # Check to make sure all items to remove are components in list_object
-    list_names <- summarize_ol(list_object)$"name"
-    invalid_names <- to_remove[!to_remove %in% list_names]
-    if (length(invalid_names) > 0) {
-        warning(
-            paste0(
-                "Did you make a typo? The following names are not present in",
-                " your data list: ", invalid_names
-            )
-        )
-    }
-    pruned_list <- list_object[!list_names %in% to_remove]
-    return(pruned_list)
 }
