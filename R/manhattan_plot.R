13fb6f068bd4ae9cb540fdcfb73f806cf28d5152
psvelayudhan <psvelayu@gmail.com>
2023-11-03 12:55:00 -0400

    vignette on manhattan plots shows how associations outside the original data can be used

--- R/manhattan_plot.R
+++ R/manhattan_plot.R
@@ -5,9 +5,15 @@
 #' @param key_mode When TRUE, generates a plot of associations relative to a
 #'  key variable (calculated previously through calculate_associations) rather
 #'  than associations against a cluster solution
+#' @param threshold P-value threshold to plot dashed line at.
+#' @param bonferroni_line If TRUE, plots a dashed black line at the
+#'  Bonferroni-corrected equivalent of the p-value threshold.
 #'
 #' @export
-manhattan_plot <- function(data, key_mode = FALSE) {
+manhattan_plot <- function(data,
+                           key_mode = FALSE,
+                           threshold = NULL,
+                           bonferroni_line = FALSE) {
     if (!key_mode) {
         # Suppress global visible binding errors during building
         row_id <- ""
@@ -39,16 +45,6 @@ manhattan_plot <- function(data, key_mode = FALSE) {
                 ),
                 alpha = 1,
                 size = 5
-            ) +
-            ggplot2::geom_hline(
-                yintercept = -log10(0.05 / nrow(data)),
-                linetype = "dashed",
-                colour = "black"
-            ) +
-            ggplot2::geom_hline(
-                yintercept = -log10(0.05),
-                linetype = "dashed",
-                colour = "red"
             ) +
             ggplot2::labs(
                 x = "Variable",
@@ -83,16 +79,6 @@ manhattan_plot <- function(data, key_mode = FALSE) {
                 alpha = 1,
                 size = 5
             ) +
-            ggplot2::geom_hline(
-                yintercept = -log10(0.05 / nrow(data)),
-                linetype = "dashed",
-                colour = "black"
-            ) +
-            ggplot2::geom_hline(
-                yintercept = -log10(0.05),
-                linetype = "dashed",
-                colour = "red"
-            ) +
             ggplot2::labs(
                 x = "Variable",
                 y = expression("-log"[10]*"(p)"),
@@ -107,6 +93,25 @@ manhattan_plot <- function(data, key_mode = FALSE) {
                 ),
                 plot.title = ggplot2::element_text(hjust = 0.5)
             )
+    }
+    if (!is.null(threshold)) {
+        plot <- plot + ggplot2::geom_hline(
+            yintercept = -log10(threshold),
+            linetype = "dashed",
+            colour = "red"
+        )
+        if (!is.null(bonferroni_line)) {
+            plot <- plot + ggplot2::geom_hline(
+                yintercept = -log10(threshold / nrow(data)),
+                linetype = "dashed",
+                colour = "black"
+            )
+        }
+    } else if (bonferroni_line) {
+        stop(
+            "Please specify threshold p-value which will be used",
+            " for calculting the Bonferroni-corrected line."
+        )
     }
     return(plot)
 }
