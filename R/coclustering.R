23b7310be1b0df7cd6771439bd416a21f93f84f3
psvelayudhan <psvelayu@gmail.com>
2024-05-13 19:31:28 -0400

    minor cleaning

--- R/coclustering.R
+++ R/coclustering.R
@@ -27,7 +27,7 @@ generate_cocluster_data <- function(data_list,
     # A list of all the cluster solutions obtained from each subsample
     ###########################################################################
     subsample_solutions <- lapply(
-        1:length(data_list_subsamples),
+        seq_along(data_list_subsamples),
         function(x) {
             solutions_matrix <- batch_snf(
                 data_list = data_list_subsamples[[x]],
@@ -51,27 +51,6 @@ generate_cocluster_data <- function(data_list,
     # Loop through all subject pairs and update the matrices
     ###########################################################################
     subject_pairs <- utils::combn(subjects, 2) # each column is a pair
-    #for (col in seq_len(ncol(subject_pairs))) {
-    #    sub1 <- subject_pairs[1, col]
-    #    sub2 <- subject_pairs[2, col]
-    #    for (s in subsample_solutions) {
-    #        sub1_cluster <- s[s$"subjectkey" == sub1, "cluster"]
-    #        sub2_cluster <- s[s$"subjectkey" == sub2, "cluster"]
-    #        both_subs_in_s <- length(sub1_cluster) + length(sub2_cluster) == 2
-    #        # If both subjects are in this subsample, add to their same
-    #        # solution count.
-    #        if (both_subs_in_s) {
-    #            same_solution[sub1, sub2] <- same_solution[sub1, sub2] + 1
-    #            same_solution[sub2, sub1] <- same_solution[sub2, sub1] + 1
-    #            # If both subjects have the same cluster in this subsample, add
-    #            # to their same cluster count.
-    #            if (sub1_cluster == sub2_cluster) {
-    #                same_cluster[sub1, sub2] <- same_cluster[sub1, sub2] + 1
-    #                same_cluster[sub2, sub1] <- same_cluster[sub2, sub1] + 1
-    #            }
-    #        }
-    #    }
-    #}
     subsample_index <- 0
     for (s in subsample_solutions) {
         subsample_index <- subsample_index + 1
