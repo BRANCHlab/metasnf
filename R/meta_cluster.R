41e081eadee06b689154e8f3f26f882ddf578ef9
psvelayudhan <psvelayu@gmail.com>
2024-05-17 16:14:39 -0400

    rename `calc_om_aris` to `calc_aris`

--- R/meta_cluster.R
+++ R/meta_cluster.R
@@ -3,7 +3,9 @@
 #' Generate matrix of pairwise cluster-solution similarities by Adjusted Rand
 #'  index calculations.
 #'
-#' @param om solutions_matrix to calculate pairwise ARIs for.
+#' @param solutions_matrix solutions_matrix containing cluster solutions to
+#' calculate pairwise ARIs for.
+#'
 #' @param processes Specify number of processes used to complete calculations
 #'  * `1` (default) Sequential processing
 #'  * `2` or higher: Parallel processing will use the
@@ -17,21 +19,21 @@
 #' @return om_aris ARIs between clustering solutions of an solutions matrix
 #'
 #' @export
-calc_om_aris <- function(om,
-                         processes = 1) {
+calc_aris <- function(solutions_matrix,
+                      processes = 1) {
     ###########################################################################
     # Prepare dataframe containing 1 cluster solution per row
     ###########################################################################
-    # Only row id and subject label columns
-    om_subs <- subs(om)
+    # Row id and subjectkey columns
+    subjects <- subs(solutions_matrix)
     # Only subject label cols
-    om_no_id <- as.matrix(om_subs[, 2:length(om_subs)])
+    subjects_no_id <- as.matrix(subjects[, 2:length(subjects)])
     # The skeleton of the inter-cluster similarity matrix
-    om_aris <- matrix(1, nrow(om_subs), nrow(om_subs))
+    aris <- matrix(1, nrow(subjects), nrow(subjects))
     ###########################################################################
     # Indices of all pairwise comparisons to calculate ARIs for
     ###########################################################################
-    pairwise_indices <- utils::combn(nrow(om_aris), 2)
+    pairwise_indices <- utils::combn(nrow(aris), 2)
     ###########################################################################
     # Run calculations (sequentially or in parallel)
     ###########################################################################
@@ -43,17 +45,20 @@ calc_om_aris <- function(om,
             }
             v1 <- pairwise_indices[1, col]
             v2 <- pairwise_indices[2, col]
-            ari <- mclust::adjustedRandIndex(om_no_id[v1, ], om_no_id[v2, ])
-            om_aris[v1, v2] <- ari
-            om_aris[v2, v1] <- ari
+            ari <- mclust::adjustedRandIndex(
+                subjects_no_id[v1, ],
+                subjects_no_id[v2, ]
+            )
+            aris[v1, v2] <- ari
+            aris[v2, v1] <- ari
         }
-        colnames(om_aris) <- om$"row_id"
-        rownames(om_aris) <- om$"row_id"
+        colnames(aris) <- solutions_matrix$"row_id"
+        rownames(aris) <- solutions_matrix$"row_id"
         cat("\r")
         cat("\n")
         cat("\r", "Done.", sep = "")
         cat("\n")
-        return(om_aris)
+        return(aris)
     } else {
         max_cores <- future::availableCores()
         if (processes == "max") {
@@ -74,8 +79,8 @@ calc_om_aris <- function(om,
             MARGIN = 2,
             FUN = function(col) {
                 mclust::adjustedRandIndex(
-                    om_no_id[col[1], ],
-                    om_no_id[col[2], ]
+                    subjects_no_id[col[1], ],
+                    subjects_no_id[col[2], ]
                 )
             }
         )
@@ -83,11 +88,11 @@ calc_om_aris <- function(om,
         #######################################################################
         # Formatting of results to symmetric matrix
         #######################################################################
-        om_aris[lower.tri(om_aris, diag = FALSE)] <- ari_vector
-        om_aris <- t(om_aris)
-        om_aris[lower.tri(om_aris)] <- t(om_aris)[lower.tri(om_aris)]
-        colnames(om_aris) <- om$"row_id"
-        rownames(om_aris) <- om$"row_id"
-        return(om_aris)
+        aris[lower.tri(aris, diag = FALSE)] <- ari_vector
+        aris <- t(aris)
+        aris[lower.tri(aris)] <- t(aris)[lower.tri(aris)]
+        colnames(aris) <- solutions_matrix$"row_id"
+        rownames(aris) <- solutions_matrix$"row_id"
+        return(aris)
     }
 }
