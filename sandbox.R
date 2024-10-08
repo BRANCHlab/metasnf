ef46ce7587b90591086b3c05577d8ac79ae8e4f5
prashanth velayudhan <psvelayu@gmail.com>
2024-10-08 10:24:30 -0400

    variation of sandbox coclustering that makes one dataframe per cluster solution

--- sandbox.R
+++ sandbox.R
@@ -223,12 +223,14 @@ subsample_pairwise_aris <- function(subsample_solutions, return_raw_aris = FALSE
 #' Average fraction of clustering together{{{
 #'
 #' Calculate average fraction of times that observations who clusted
-#'  together in the full solution continued to cluster together in all the
-#'  subsampled solutions.
+#' together in the full solution continued to cluster together in all the
+#' subsampled solutions.
 #'
 #' @param data_list_subsamples A list of subsets of a larger data_list. See
-#'  ?subsample_data_list to obtain this object.
+#' ?subsample_data_list to obtain this object.
+#'
 #' @param settings_matrix A settings_matrix.
+#'
 #' @param solutions_matrix A solutions_matrix.
 #'
 #' @return fraction_together_df Dataframe containing the average fraction
@@ -403,7 +405,8 @@ cocluster_density <- function(cocluster_counts) {# {{{
         cocluster_counts$"original_cluster"
     )
     missing <- sum(is.na(cocluster_counts$"frac_together"))
-    if (missing > 0) {
+    # Coverage check
+    if (missing > 0) {# {{{
         cocluster_counts <- na.omit(cocluster_counts)
         warning(
             paste0(
@@ -416,7 +419,7 @@ cocluster_density <- function(cocluster_counts) {# {{{
                 " `subsample_data_list()`."
             )
         )
-    }
+    }# }}}
     dist_plot <- cocluster_counts |>
         ggplot2::ggplot(
             ggplot2::aes(
@@ -473,32 +476,190 @@ ari_summary_h <- pairwise_aris_h$"ari_summary"
 raw_aris_h <- pairwise_aris_h$"raw_aris"
 
 # The manual formula converting an ARI matrix to its average reported value
-((sum(as.numeric(pairwise_aris$"raw_aris"$"row_4")) - 7)/2)/21
+((sum(as.numeric(pairwise_aris_l$"raw_aris"$"row_4")) - 7)/2)/21
 
 # scales linearly with the number of subsamples and quadratically with the number of
 # observations.
 
-coclustering_results_l <- calculate_coclustering(
-    data_list_subsamples_l,
-    settings_matrix,
+#coclustering_results_l <- calculate_coclustering(
+#    data_list_subsamples_l,
+#    settings_matrix,
+#    subsample_cluster_solutions_l,
+#    solutions_matrix,
+#    keep_raw = TRUE
+#)
+#
+#coclustering_results_h <- calculate_coclustering(
+#    data_list_subsamples_h,
+#    settings_matrix,
+#    subsample_cluster_solutions_h,
+#    solutions_matrix,
+#    keep_raw = TRUE
+#)
+
+calc_coc2 <- function(subsample_solutions,
+                      solutions_matrix,
+                      keep_raw = FALSE) {
+    # For dplyr
+    cluster <- ""
+    sub_1_clust <- ""
+    sub_2_clust <- ""
+    # Data frame containing the cluster solutions from the full data_list
+    full_cluster_solutions <- get_cluster_solutions(solutions_matrix)
+    # Data frame that will store summary data
+    cocluster_frac_df <- data.frame()
+    # List that will optionally track raw coclustering data
+    cocluster_dfs <- list()
+    # Looping over all cluster solutions
+    solution_indices <- seq_len(nrow(solutions_matrix))
+    for (idx in solution_indices) {
+        # Print current solution for monitoring progress
+        print(
+            paste0(
+                "Calculating coclustering fraction for solution ",
+                idx, "/", max(solution_indices), "..."
+            )
+        )
+        current_solution_df <- full_cluster_solutions[, c(1, idx + 1)]
+        colnames(current_solution_df) <- c("subjectkey", "cluster")
+        # Unique clusters that exist in this solution
+        unique_clusters <- sort(unique(current_solution_df$"cluster"))
+        # Dataframe storing all pairs of subjects in the full solution
+        cocluster_df <- current_solution_df$"subjectkey" |>
+            utils::combn(2) |>
+            t() |>
+            data.frame()
+        colnames(cocluster_df) <- c("sub_1", "sub_2")
+        cocluster_df <- dplyr::left_join(
+            cocluster_df,
+            current_solution_df,
+            dplyr::join_by(sub_1 == subjectkey)
+        )
+        cocluster_df <- dplyr::left_join(
+            cocluster_df,
+            current_solution_df,
+            dplyr::join_by(sub_2 == subjectkey)
+        )
+        colnames(cocluster_df) <- c(
+            "sub_1", "sub_2", "sub_1_clust", "sub_2_clust"
+        )
+        cocluster_df$"same_solution" <- 0
+        cocluster_df$"same_cluster" <- 0
+        # Optionally initialize raw data matrices
+        # Iteration through all the clustered pairs
+        for (row in seq_len(nrow(cocluster_df))) {
+            print(row)
+            # Iteration through all the solutions of subsampled data
+            for (sub_ind in seq_len(length(subsample_solutions))) {
+                subsample <- subsample_solutions[[sub_ind]]
+                subsample_subjects <- subsample$"subjectkey"
+                # df with subjectkey and only the current cluster solution
+                current_ss <- subsample[, c(1, idx + 1)]
+                colnames(current_ss) <- c("subjectkey", "cluster")
+                rownames(current_ss) <- current_ss$"subjectkey"
+                sub_1 <- cocluster_df[row, "sub_1"]
+                sub_2 <- cocluster_df[row, "sub_2"]
+                ss_has_sub_1 <- sub_1 %in% current_ss$"subjectkey"
+                ss_has_sub_2 <- sub_2 %in% current_ss$"subjectkey"
+                if (ss_has_sub_1 & ss_has_sub_2) {
+                    cocluster_df[row, "same_solution"] <-
+                        cocluster_df[row, "same_solution"] + 1
+                    ss_sub_1_c <- current_ss[sub_1, "cluster"]
+                    ss_sub_2_c <- current_ss[sub_2, "cluster"]
+                    if (ss_sub_1_c == ss_sub_2_c) {
+                        cocluster_df[row, "same_cluster"] <-
+                            cocluster_df[row, "same_cluster"] + 1
+                    }
+                }
+            }
+        }
+        incomplete_coverage <- sum(cocluster_df$"same_solution" == 0)
+        if (incomplete_coverage > 0) {
+            warning(
+                paste0(
+                    incomplete_coverage,
+                    " out of ", nrow(cocluster_df), " originally",
+                    " co-clustered pairs of observations did not appear in",
+                    " any of the data list subsamples together. Estimates",
+                    " of co-clustering quality may be skewed as a result.",
+                    " Consider increasing the value of the",
+                    " `subsample_fraction` or",
+                    " `n_subsamples` arguments when calling",
+                    " `subsample_data_list()`."
+                )
+            )
+        }
+        original_cocluster_df <- cocluster_df |>
+            dplyr::filter(sub_1_clust == sub_2_clust) |>
+            dplyr::mutate(cocluster_frac = same_cluster / same_solution)
+        avg_cocluster_frac <- mean(
+            original_cocluster_df$"cocluster_frac", na.rm = TRUE
+        )
+        cocluster_frac_df <- rbind(
+            cocluster_frac_df,
+            data.frame(
+                "row" = idx,
+                "avg_cocluster_frac" = avg_cocluster_frac
+            )
+        )
+        if (keep_raw) {
+            idx <- length(cocluster_dfs) + 1
+            cocluster_dfs[[idx]] <- cocluster_df
+        }
+    }
+    if (keep_raw) {
+        results <- list(
+            "cocluster_df" = cocluster_dfs,
+            "cocluster_summary" = cocluster_frac_df
+        )
+        return(results)
+    } else {
+        return(cocluster_frac_df)
+    }
+}
+
+coclustering_results_l <- calc_coc2(
     subsample_cluster_solutions_l,
     solutions_matrix,
     keep_raw = TRUE
 )
 
-coclustering_results_h <- calculate_coclustering(
-    data_list_subsamples_h,
-    settings_matrix,
-    subsample_cluster_solutions_h,
-    solutions_matrix,
-    keep_raw = TRUE
+subsample_cluster_solutions_l
+
+length(subsample_cluster_solutions_l)
+
+length(data_list_subsamples_l)
+
+no_same <- lapply(
+    coclustering_results_l$"cocluster_df",
+    function(x) {
+        dplyr::filter(x, same_solution == 0)
+    }
 )
 
-# "cocluster_count_dfs" and "cocluster summary"
-names(coclustering_results)
+lapply(no_same, nrow)
 
+no_same[[1]]
+
+identical(no_same[[1]], no_same[[2]])
+
+z <- collapse_dl(data_list_subsamples_l[[1]])
+
+z
+
+
+
+no_same_1$"sub_1" %in% z$"subjectkey"
+
+no_same_1$"sub_2" %in% z$"subjectkey"
+
+"subject_NDAR_INV0J4PYA5F" %in% z$"subjectkey"
+
+# "cocluster_count_dfs" and "cocluster summary"
 cocluster_count_dfs_l <- coclustering_results_l$"cocluster_count_dfs"
 cocluster_count_dfs_h <- coclustering_results_h$"cocluster_count_dfs"
+
+head(cocluster_count_dfs_h[[1]])
 
 cocluster_density(cocluster_count_dfs_l[[1]])
 cocluster_density(cocluster_count_dfs_h[[1]])
@@ -572,7 +733,6 @@ cocluster_heatmap2(
     cocluster_count_dfs_h[[1]],
     solutions_matrix[1, ]
 )
-
 
 cocluster_heatmap2(
     cocluster_count_dfs_l[[1]],
