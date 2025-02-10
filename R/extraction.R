0f2fa8157b9fd87719e5b5cbba6651d81279f1df
psvelayudhan <psvelayu@gmail.com>
2025-02-10 10:18:12 -0500

    bugfix: slicing SNF config made weights matrix lose its class

--- R/extraction.R
+++ R/extraction.R
@@ -60,6 +60,17 @@
 #' @export
 `[.weights_matrix` <- function(x, i, j, ...) {
     result <- NextMethod("[")
+    class(result) <- setdiff(class(result), "weights_matrix")
+    result <- tryCatch(
+        expr = {
+            result <- validate_weights_matrix(result)
+            result <- new_weights_matrix(result)
+            result
+        },
+        error = function(e) {
+            result
+        }
+    )
     return(result)
 }
 
