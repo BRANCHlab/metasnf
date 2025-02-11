9b766cf56c8ca34d5e6f5c6f4e7a7256d7f4d007
psvelayudhan <psvelayu@gmail.com>
2025-02-11 15:27:22 -0500

    build commit

--- sandbox.R
+++ sandbox.R
@@ -1,3 +1,8 @@
+# To-do
+# - Ensure attributes of solutions df and ext solutions df are preserved during extractions
+#   for example, features attribute becomes lost during something like:
+#   attr(mtbi_ext_sol_df_p1[, 1:200], "features")
+
 devtools::load_all()
 
 input_dl <- data_list(
