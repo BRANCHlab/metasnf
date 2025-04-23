4515017710cd1461b3aac1c9abad819ca8dbc3e1
psvelayudhan <psvelayu@gmail.com>
2025-04-23 17:04:44 -0400

    mark features() as internal

--- R/features.R
+++ R/features.R
@@ -1,5 +1,6 @@
 #' Return character vector of features stored in an object
 #'
+#' @keywords internal
 #' @param x The object to pull features from.
 #' @return A character vector of features in x.
 #' @export
@@ -7,16 +8,19 @@ features <- function(x) {
     UseMethod("features")
 }
 
+#' @keywords internal
 #' @export
 features.data_list <- function(x) {
     return(attributes(x)$"features")
 }
 
+#' @keywords internal
 #' @export
 features.solutions_df <- function(x) {
     return(attributes(x)$"features")
 }
 
+#' @keywords internal
 #' @export
 features.ext_solutions_df <- function(x) {
     return(attributes(x)$"features")
