af426a93d4728095e8887575b0594c68302e8de4
psvelayudhan <psvelayu@gmail.com>
2025-04-23 17:05:02 -0400

    add variety of assignment methods

--- R/assignment.R
+++ R/assignment.R
@@ -1,19 +1,11 @@
 #' @export
-`[<-.data_list` <- function(x, i, value) {
-    dll <- NextMethod()
-    validate_data_list(dll)
-    dl <- as_data_list(dll)
-    return(dl)
-}
-
-#' @export
-`[<-.settings_df` <- function(x, i, j, value) {
+`[<-.ari_matrix` <- function(x, i, j, value) {
     result <- NextMethod()
-    class(result) <- setdiff(class(result), "settings_df")
+    class(result) <- setdiff(class(result), "ari_matrix")
     result <- tryCatch(
         expr = {
-            result <- validate_settings_df(result)
-            result <- new_settings_df(result)
+            result <- validate_ari_matrix(result)
+            result <- new_ari_matrix(result)
             result
         },
         error = function(e) {
@@ -24,13 +16,38 @@
 }
 
 #' @export
-`[<-.solutions_df` <- function(x, i, j, value) {
+`[<-.clust_fns_list` <- function(x, i, j, value) {
     result <- NextMethod()
-    class(result) <- setdiff(class(result), "solutions_df")
+    class(result) <- "list"
     result <- tryCatch(
         expr = {
-            result <- validate_solutions_df(result)
-            result <- new_solutions_df(result)
+            result <- validate_clust_fns_list(result)
+            result <- new_clust_fns_list(result)
+            result
+        },
+        error = function(e) {
+            result
+        }
+    )
+    return(result)
+}
+
+#' @export
+`[<-.data_list` <- function(x, i, value) {
+    dll <- NextMethod()
+    validate_data_list(dll)
+    dl <- as_data_list(dll)
+    return(dl)
+}
+
+#' @export
+`[<-.dist_fns_list` <- function(x, i, j, value) {
+    result <- NextMethod()
+    class(result) <- "list"
+    result <- tryCatch(
+        expr = {
+            result <- validate_dist_fns_list(result)
+            result <- new_dist_fns_list(result)
             result
         },
         error = function(e) {
@@ -58,6 +75,86 @@
 }
 
 #' @export
+`[<-.settings_df` <- function(x, i, j, value) {
+    result <- NextMethod()
+    class(result) <- setdiff(class(result), "settings_df")
+    result <- tryCatch(
+        expr = {
+            result <- validate_settings_df(result)
+            result <- new_settings_df(result)
+            result
+        },
+        error = function(e) {
+            result
+        }
+    )
+    return(result)
+}
+
+#' @export
+`[<-.sim_mats_list` <- function(x, i, value) {
+    result <- NextMethod()
+    class(result) <- "list"
+    result <- tryCatch(
+        expr = {
+            result <- validate_sim_mats_list(result)
+            result <- new_sim_mats_list(result)
+            result
+        },
+        error = function(e) {
+            result
+        }
+    )
+    return(result)
+}
+
+#' @export
+`[<-.solutions_df` <- function(x, i, j, value) {
+    result <- NextMethod()
+    class(result) <- setdiff(class(result), "solutions_df")
+    result <- tryCatch(
+        expr = {
+            result <- validate_solutions_df(result)
+            result <- new_solutions_df(result)
+            result
+        },
+        error = function(e) {
+            result
+        }
+    )
+    return(result)
+}
+
+#' @export
+`[<-.snf_config` <- function(x, i, j, value) {
+    result <- NextMethod()
+    class(result) <- "list"
+    result <- tryCatch(
+        expr = {
+            result <- validate_snf_config(result)
+            result <- new_snf_config(result)
+            result
+        },
+        error = function(e) {
+            result
+        }
+    )
+    return(result)
+}
+
+#' @export
+`[<-.t_solutions_df` <- function(x, i, j, value) {
+    result <- NextMethod()
+    result
+}
+
+#' @export
+`[<-.t_ext_solutions_df` <- function(x, i, j, value) {
+    result <- NextMethod()
+    result
+}
+
+#' @export
 `[<-.weights_matrix` <- function(x, i, j, value) {
     result <- NextMethod()
     class(result) <- setdiff(class(result), "weights_matrix")
@@ -72,27 +169,4 @@
         }
     )
     return(result)
-}
-
-#' @export
-`[<-.ari_matrix` <- function(x, i, j, value) {
-    result <- NextMethod()
-    class(result) <- setdiff(class(result), "ari_matrix")
-    result <- tryCatch(
-        expr = {
-            result <- validate_ari_matrix(result)
-            result <- new_ari_matrix(result)
-            result
-        },
-        error = function(e) {
-            result
-        }
-    )
-    return(result)
-}
-
-#' @export
-`[<-.sim_mats_list` <- function(x, i, value) {
-    cat("cake")
-    NextMethod()
 }
