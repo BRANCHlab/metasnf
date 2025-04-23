8436a0a47d83d7dc1fdf2a239ab8a45f0c642a6e
psvelayudhan <psvelayu@gmail.com>
2025-04-23 17:03:56 -0400

    summary_features marked as internal

--- R/summary_features.R
+++ R/summary_features.R
@@ -1,5 +1,6 @@
 #' Pull features used to calculate summary p-values from an object
 #'
+#' @keywords internal
 #' @param x The object to extract summary features from.
 #' @return A character vector of summary features.
 #' @export
@@ -7,6 +8,7 @@ summary_features <- function(x) {
     UseMethod("summary_features")
 }
 
+#' @keywords internal
 #' @export
 summary_features.ext_solutions_df <- function(x) {
     summary_fts <- attributes(x)$"summary_features"
