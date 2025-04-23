f9183b1ddd1b398f4119cf58ab87d543d42bd59c
psvelayudhan <psvelayu@gmail.com>
2025-04-23 17:04:33 -0400

    mark n_features() as internal

--- R/n_features.R
+++ R/n_features.R
@@ -1,5 +1,6 @@
 #' Extract number of features stored in an object
 #'
+#' @keywords internal
 #' @param x The object to extract number of features from.
 #' @return The number of features in x.
 #' @export
@@ -7,6 +8,7 @@ n_features <- function(x) {
     UseMethod("n_features")
 }
 
+#' @keywords internal
 #' @export
 n_features.data_list <- function(x) {
     return(attributes(x)$"n_features")
