fe44cb4b7b45ae55bfd2e3968ea30d23f2c6b2d8
psvelayudhan <psvelayu@gmail.com>
2025-04-23 17:04:27 -0400

    mark observations() as internal

--- R/n_observations.R
+++ R/n_observations.R
@@ -1,5 +1,6 @@
 #' Extract number of observations stored in an object
 #'
+#' @keywords internal
 #' @param x The object to extract number of observations from.
 #' @return The number of observations in x.
 #' @export
@@ -7,6 +8,7 @@ n_observations <- function(x) {
     UseMethod("n_observations")
 }
 
+#' @keywords internal
 #' @export
 n_observations.data_list <- function(x) {
     return(attributes(x)$"n_observations")
